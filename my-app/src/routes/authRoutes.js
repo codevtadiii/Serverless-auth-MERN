6564fbe514a6337e6927f9ce7639db6f44794257
Aditya jha <medoremon0001@gmail.com>
2025-02-23 12:38:05 +0530

    Create authRoutes.js

---
+++ my-app/src/routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/User'); // Import User model
+
+const router = express.Router();
+
+
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user', error });
+  }
+});
+
+module.exports = router;
