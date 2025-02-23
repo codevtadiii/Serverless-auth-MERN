bbb548b0693f327783a66311ca5ad000fc092e42
Aditya jha <medoremon0001@gmail.com>
2025-02-23 12:38:20 +0530

    Create productpage.js

---
+++ my-app/src/pages/productpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Product = ({ product, addToCart }) => {
+    return (
+        <div className="product">
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>${product.price.toFixed(2)}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
