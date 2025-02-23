06f349c20e158576372e35a13e188991ef323110
Aditya jha <medoremon0001@gmail.com>
2025-02-23 12:38:34 +0530

    Create checkout.js

---
+++ my-app/src/pages/checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>Review your items and complete your purchase.</p>
+    </div>
+  );
+}
+
+export default Checkout;
