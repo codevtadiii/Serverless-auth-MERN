49c8b869e2d80879a350e030c463b234f1554de1
Aditya jha <medoremon0001@gmail.com>
2025-05-29 01:08:21 +0530

    Update home.js

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -5,11 +5,14 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Welcome to the Blood Donation App</h1>
+    <div>
+      <h1>Need Help? </h1>
+      <br>
+      </br>
+      
       <button
         onClick={() => navigate("/request-form")}
-        className="px-6 py-3 bg-blue-500 text-white font-bold rounded-md shadow-md hover:bg-blue-700"
+        className="button"
       >
         Raise a Request
       </button>
